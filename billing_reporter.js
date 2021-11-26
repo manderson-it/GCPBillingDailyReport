d16efee2a403ce2a500122684433968bcbfd5f35
Marek Anderson <marek.anderson@sada.com>
2021-11-26 16:23:03 -0500

    fix formatting

--- billing_reporter.js
+++ billing_reporter.js
@@ -33,7 +33,7 @@ ORDER BY
   day desc
 ;
 `;
-const billingReport = data => data[0].map(row => `${row.day}:${row.total}`).join('\n');
+const billingReport = data => data[0].map(row => `${row.day} ${row.total}`).join('\n');
 
 class BillingReporter {
 
