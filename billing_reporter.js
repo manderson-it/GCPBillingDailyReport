f60f6ad0af02fac2d0e96ba99bcc4e9172f6a73d
Marek Anderson <marek.anderson@sada.com>
2021-11-30 14:43:27 -0500

    query with project labels

--- billing_reporter.js
+++ billing_reporter.js
@@ -1,36 +1,23 @@
 const { BigQuery } = require('@google-cloud/bigquery');
-/*
+
 const query = (billingAccountId,billingDataset) => `
 SELECT
-  invoice.month,
-  SUM(cost)
-    + SUM(IFNULL((SELECT SUM(c.amount)
-                  FROM UNNEST(credits) c), 0))
-    AS total,
-  (SUM(CAST(cost * 1000000 AS int64))
-    + SUM(IFNULL((SELECT SUM(CAST(c.amount * 1000000 as int64))
-                  FROM UNNEST(credits) c), 0))) / 1000000
-    AS total_exact
-FROM \`${billingDataset}.gcp_billing_export_resource_v1_${billingAccountId}\`
-GROUP BY 1
-ORDER BY 1 ASC
-;
-`;
-*/
-const query = (billingAccountId,billingDataset) => `
-SELECT
-  ROUND(SUM(cost),2) AS total,
-  FORMAT_DATE('%Y-%m-%d', DATE(usage_end_time)) AS day
+  ROUND(SUM(cost),2) AS dailyCost,
+  FORMAT_DATE('%Y-%m-%d', DATE(usage_end_time)) AS day,
+  (
+      SELECT value
+      FROM UNNEST(project.labels)
+      WHERE KEY='env') AS environment
 FROM \`${billingDataset}.gcp_billing_export_resource_v1_${billingAccountId}\`
 WHERE
-  cost_type = "regular" AND
   FORMAT_DATE('%Y-%m-%d', DATE(usage_end_time)) = FORMAT_DATE('%Y-%m-%d', CURRENT_DATE()) OR 
   FORMAT_DATE('%Y-%m-%d', DATE(usage_end_time)) = FORMAT_DATE('%Y-%m-%d', DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY)) OR
   FORMAT_DATE('%Y-%m-%d', DATE(usage_end_time)) = FORMAT_DATE('%Y-%m-%d', DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY))
-GROUP BY 
+GROUP BY
+  environment,
   day
 ORDER BY
-  day desc
+  day DESC
 ;
 `;
 const billingReport = data => data[0].map(row => `${row.day} ${row.total}`).join('\n');
